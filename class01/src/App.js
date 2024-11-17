78350a60a927ee697a64ec21031151db4bf355b1
fanubalti786 <fanubalti786@gmail.com>
2024-11-17 16:10:35 +0500

    update

--- class01/src/App.js
+++ class01/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./header";
+import Footer from "./footer";
 
 function App(){
   return(
     <div>
       <Header/>
-      <h1>hello</h1>
+      <h1>App.js</h1>
+      <Footer/>
     </div>
   )
 }
