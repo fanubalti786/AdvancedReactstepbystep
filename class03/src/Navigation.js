8306565e4b52924e9240ecf6b51ed4abaca91540
fanubalti786 <fanubalti786@gmail.com>
2024-11-18 17:55:59 +0500

    Routing logic Done with Routing Navigation folder import package and run default code wich provide me react rounter dom

--- class03/src/Navigation.js
+++ class03/src/Navigation.js
@@ -1,17 +1,36 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
 import React from 'react'
 import Home from "./pages/home/Home"
 import About from './pages/about/About'
 import ContactUs from './pages/contactUs/ContactUs'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+
+  {
+    path: "/About",
+    element: <About/>,
+  },
+
+  {
+    path: "/ContactUs",
+    element: <ContactUs/>,
+  },
+]);
 
 export default function Navigation() {
+
   return (
     <div>
-      <Home/>
-      <About/>
-      <ContactUs/>
-
-      
+      <RouterProvider router={router} />
     </div>
+    
   )
 }
