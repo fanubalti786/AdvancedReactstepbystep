43260a20d1cadc77cd31342fa118e55d627fd04a
fanubalti786 <fanubalti786@gmail.com>
2024-11-18 17:20:45 +0500

    update

---
+++ class03/src/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Home from "./pages/home/Home"
+import About from './pages/about/About'
+import ContactUs from './pages/contactUs/ContactUs'
+
+
+export default function Navigation() {
+  return (
+    <div>
+      <Home/>
+      <About/>
+      <ContactUs/>
+
+      
+    </div>
+  )
+}
