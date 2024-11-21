ab0e2c44068aa0f6e8f56c01d8989eee3ad48441
fanubalti786 <fanubalti786@gmail.com>
2024-11-21 20:26:36 +0500

    update

---
+++ class04/src/constant/data.js
@@ -0,0 +1,36 @@
+const data = [
+ {
+    id:1,
+    name:"irfan",
+    email:"fanubalti796@gmail.com",
+    rolno:21432,
+    class:"BSSE"
+ },
+
+ {
+    id:1,
+    name:"irfan",
+    email:"fanubalti796@gmail.com",
+    rolno:21432,
+    class:"BSSE"
+ }, 
+
+
+ {
+    id:1,
+    name:"irfan",
+    email:"fanubalti796@gmail.com",
+    rolno:21432,
+    class:"BSSE"
+ },  
+
+
+
+ {
+    id:1,
+    name:"irfan",
+    email:"fanubalti796@gmail.com",
+    rolno:21432,
+    class:"BSSE"
+ }   
+]
---
+++ class04/src/pages/home/Home.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <table>
+        <tr>
+            <th>
+                Id:
+            </th>
+            <th>
+                Name:
+            </th>
+            <th>
+                Email:
+            </th>
+            <th>
+                Rollno:
+            </th>
+            <th>
+                class:
+            </th>
+        </tr>
+        
+        <hr/>
+
+        <tr>
+            <td>
+                1
+            </td>
+            <td>
+                irfan
+            </td>
+            <td>
+                fanubalti796
+            </td>
+            <td>
+                21432
+            </td>
+            <td>
+                BSSE
+            </td>
+            
+
+            
+
+        </tr>
+      </table>
+    </div>
+  )
+}
