a009bea3a5ad6bd021f43fd8026e2b354a5c6adb
fanubalti786 <fanubalti786@gmail.com>
2024-11-26 13:07:49 +0500

    update

---
+++ class07/src/constant/Constant.js
@@ -0,0 +1,7 @@
+export const student = {
+    name: "irfan",
+    rolno: 21432,
+    email: "fanubalti786@gmail.com"
+}
+
+
---
+++ class07/src/pages/home/Home.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { student } from '../../constant/Constant'
+export default function Home() {
+
+    // let newStudent = {...student}; // its not a reference variable
+    // let newStudent = student;  // its a reference variable
+    // newStudent.name = "hello";
+    // newStudent.map(()=>
+    // {
+
+    // })
+
+
+    //Not Possible i don't know why
+    // student = {
+    //     ...student,
+    //     geneder: "male",
+    //     name: "sohail"
+        
+    // }
+
+    //we can do update and add keys through this logic
+    // let newStudent= {
+    //     ...student,
+    //     geneder: "male",
+    //     name: "sohail"
+        
+    // }
+
+
+    let fuc = (a)=>
+    {
+        console.log("Value is ", a)
+    }
+
+    let fuc1 = fuc;
+    fuc1(5);
+
+    console.log(student)
+
+  return (
+    <div>
+      <h1>{student.name}</h1>
+      <h1>{student.rolno}</h1>
+      <h1>{student.email}</h1>
+
+     
+
+
+    </div>
+  )
+}
