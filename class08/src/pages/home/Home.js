e533955f057d600f7bf5a5b893148b2249e5036e
fanubalti786 <fanubalti786@gmail.com>
2024-11-29 21:00:58 +0500

    update

--- class08/src/pages/home/Home.js
+++ class08/src/pages/home/Home.js
@@ -1,55 +1,50 @@
-import React from 'react'
-import { useSelector,useDispatch } from 'react-redux'
-import { fetchProduct } from '../../store/Slices/ProductSlice';
-
-
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchProduct } from "../../store/Slices/ProductSlice";
 
 export default function Home() {
-
-  const product = useSelector(state=>state.productSlice.products);
+  const product = useSelector((state) => state.productSlice.products);
   console.log(product);
 
   const dispatch = useDispatch();
-    
-    const GetData = ()=>
-    {
 
-      dispatch(fetchProduct());
-      
-
-    }
-
-   
-
+  const GetData = () => {
+    dispatch(fetchProduct());
+  };
 
   return (
     <div>
-    
+      <button onClick={GetData}>GetData!</button>
 
-      <button onClick={GetData} >GetData!</button>
+      {product.length > 0 ? (
+        product?.map((item) => {
+          return (
+            <div style={{display:'flex'}}>
+              <div style={{padding:10}}>
+                <img
+                  style={{ width: 100 }}
+                  src={item.image}
+                  alt={product.title}
+                />
+              </div>
 
-      {product.length > 0?  product?.map((item)=>
-      {
-        return(
-          <div>
-          <h1>{item.id}</h1>
-          <h1>{item.title}</h1>
-          <img style={{width:100}} src={item.image} alt={product.title} />
-          <h1>{item.price}</h1>
-          <h1>{item.description}</h1>
+              <div>
+                <h1>{item.title}</h1>
+                <p>{item.description}</p>
+                <p>{item.price}</p>
+                <div style={{display:'flex'}}>
+                <button>Delete</button>
+                <button>Edit</button>
+                </div>
+                <hr/>
 
-
-          </div>
-
-        )
-      }):
-      <h1>Not Available Data</h1>
-      }
-    
-
-     
-
-
+              </div>
+            </div>
+          );
+        })
+      ) : (
+        <h1>Not Available Data</h1>
+      )}
     </div>
-  )
+  );
 }
