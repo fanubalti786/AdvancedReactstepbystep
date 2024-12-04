b2b5bdb72b49904ef88a62b9721ec56742212b28
fanubalti786 <fanubalti786@gmail.com>
2024-12-04 22:51:51 +0500

    update

--- class09/src/App.js
+++ class09/src/App.js
@@ -3,8 +3,7 @@ import Routing from './routing/Routing'
 // import { useSelector } from 'react-redux'
 
 export default function App() {
-  // const product = useSelector(store=> store.product)
-  // console.log(product)
+  
   return (
     <div>
       <Routing/>
