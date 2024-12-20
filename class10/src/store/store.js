d12a37dd6cc54b781bbab2c50faad4406738b261
fanubalti786 <fanubalti786@gmail.com>
2024-12-21 00:06:11 +0500

    Ecommerce Project with firebase

--- class10/src/store/store.js
+++
@@ -1,7 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import UsersEnroll from './Slices/UsersEnroll';
-export const store = configureStore({
-    reducer:{
-        UsersEnroll: UsersEnroll
-    }
-});
