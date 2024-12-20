da64edea03cf2135d1a94da27b171d6b44587a35
fanubalti786 <fanubalti786@gmail.com>
2024-12-21 03:10:50 +0500

    project folder structure and installation tailwind react-router-dom and layout out folder with Homepage

--- EcommerceProject/vite-project/src/App.jsx
+++ EcommerceProject/vite-project/src/App.jsx
@@ -1,12 +1,22 @@
-import { Button } from '@mui/material'
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import HomePage from './pages/home/HomePage';
+import NoPage from './pages/noPage/NoPage';
+// import { Button } from '@mui/material'
+
 
 export default function App() {
   return (
     <div>
-      MUI 
-      <br/>
-      <Button>Submit</Button>
+      <Router>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/*' element={<NoPage />} >
+
+
+          </Route>
+        </Routes>
+      </Router>
     </div>
   )
 }
--- EcommerceProject/vite-project/src/pages/home/HomePage.jsx
+++ EcommerceProject/vite-project/src/pages/home/HomePage.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-
-export default function Home() {
+export default function HomePage() {
   return (
     <div>
       Home
