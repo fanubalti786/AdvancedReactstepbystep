cae80767a2be36f3ddf5f81b7e11c84274622964
fanubalti786 <fanubalti786@gmail.com>
2025-02-11 07:55:30 +0500

    update

--- class06/src/components/testing/Testing.jsx
+++ class06/src/components/testing/Testing.jsx
@@ -4,11 +4,15 @@ export default function Testing({value}) {
     const [values,setValues] = useState("")
     useEffect(()=>
     {
+      alert("useEffect called")
         setValues(value)
-    })
+      console.log("useEffect called")
+
+    },[])
+
   return (
     <div>
-      Testing
+      <h1>Testing</h1>
       {values}
       <br />
       {value}
--- class06/src/pages/test/Test.jsx
+++ class06/src/pages/test/Test.jsx
@@ -6,6 +6,7 @@ export default function Test() {
     const [value,setValue] = useState('')
   return (
     <div>
+      <h1>Test</h1>
       <Testing value={value}/>
       <button onClick={()=>setValue("hello")}>click</button>
     </div>
