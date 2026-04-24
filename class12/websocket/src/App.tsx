8daf3a5ca2ab43684704f948a52cf5b14585ba69
fanubalti786 <fanubalti786@gmail.com>
2026-04-24 10:09:10 +0500

    first ping pong live conversation wwith websocket using frentend and backend

--- class12/websocket/src/App.tsx
+++ class12/websocket/src/App.tsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import { useEffect, useRef, useState } from "react";
 
 export default function App() {
+  const [socket, setSocket] = useState();
+  //@ts-ignore
+  const inputRef = useRef();
+
+  function sendMessage() {
+    if (!socket) {
+      return;
+    }
+    //@ts-ignore
+    const message = inputRef.current.value;
+    //@ts-ignore
+    socket.send(message);
+  }
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8080");
+    //@ts-ignore
+    setSocket(ws);
+    ws.onmessage = (ev) => {
+      console.log("message : ", ev.data);
+      alert(ev.data);
+    };
+
+    // ws.onerror = () => {
+
+    // }
+
+    // ws.close = () => {
+
+    // }
+
+    // ws.onopen = () => {
+
+    // }
+  }, []);
+
   return (
-    <div className='border-2 bg-red-800'>
-      hello
+    <div className="flex justify-center items-center gap-2">
+      <input
+        //@ts-ignore
+        ref={inputRef}
+        className="border px-1 py-1"
+        type="text"
+        placeholder="Message..."
+      />
+      <button
+        className="border px-2 py-1 cursor-pointer bg-blue-400"
+        onClick={sendMessage}
+      >
+        Send
+      </button>
     </div>
-  )
+  );
 }
